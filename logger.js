277db0eeb74247d9a4f18722c0af087024a900d2
Aleksei Belousov <vs.oko@rambler.ru>
2022-09-07 19:43:39 +0300

    added README.md

---
+++ logger.js
@@ -0,0 +1,6 @@
+const logger = require('pino')({
+    name: 'nodejs_20220421_workshop'
+});
+
+module.exports = logger;
+
---
+++ server.js
@@ -0,0 +1,43 @@
+const Koa = require('koa');
+const pug = require('pug');
+const path = require('path');
+const Router = require('koa-router');
+const sendMessage = require('./sendMessage');
+const search = require('./wikipedia');
+const config = require('./config');
+const logger = require('./logger');
+
+
+const app = new Koa();
+const router = new Router();
+
+app.use(require('koa-bodyparser')());
+
+app.use((ctx, next) => {
+    ctx.logger = logger;
+})
+
+router.post(config.bot_token, async (ctx, next) => {
+
+    try {
+        const {message: {text, chat: {id: chatId}}} = ctx.request.body;
+
+        const results = await search (text);
+
+        const msg = pug.renderFile (path.join (__dirname, 'message.pug'), {
+            results,
+        })
+        await sendMessage (chatId, msg);
+
+    } catch (err) {
+        logger.error(err)
+    }
+
+    ctx.body = 'ok';
+})
+
+//https://api.telegram.org/bot5640975958:AAEeH9_Aem1ea4bpMOjGAq14LOEmKopM7ho/sendMessage
+
+
+app.listen(config.port);
+
